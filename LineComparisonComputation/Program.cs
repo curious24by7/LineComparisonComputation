2d8619e45a34ef4e87a1f18a6d8f6fae9bab2aca
sohamdhamnaskar <sohamdham123@gmail.com>
2021-05-05 16:13:25 +0530

    [soham]Added: Calculate length for 1 line.

--- LineComparisonComputation/Program.cs
+++ LineComparisonComputation/Program.cs
@@ -7,6 +7,19 @@ namespace LineComparisonComputation
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Line Comparison Computation.");
+            Console.WriteLine("Enter x, y coordinates of the line 1::");
+            Console.WriteLine("X1");
+            int X1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("X2");
+            int X2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Y1");
+            int Y1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Y2");
+            int Y2 = Convert.ToInt32(Console.ReadLine());
+
+            double L1 = new double();
+            L1 = Math.Sqrt(Math.Pow((X2 - X1),2) + Math.Pow((Y2 - Y1),2));
+            Console.WriteLine("Length of line 1 is:: "+ L1);
         }
     }
 }
