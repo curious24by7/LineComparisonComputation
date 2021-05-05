afcbf56babda3627f720754e454115cb221336f4
sohamdhamnaskar <sohamdham123@gmail.com>
2021-05-05 16:42:55 +0530

    [soham]Added: Compareto method for both lines.

--- LineComparisonComputation/Program.cs
+++ LineComparisonComputation/Program.cs
@@ -25,22 +25,35 @@ namespace LineComparisonComputation
             int B1 = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("B2");
             int B2 = Convert.ToInt32(Console.ReadLine());
-
             double L1 = Math.Sqrt(Math.Pow((X2 - X1),2) + Math.Pow((Y2 - Y1),2));
-            Console.WriteLine("Length of line 1 is:: "+ L1);
+            Console.WriteLine("Length of L1 is:: "+ L1);
             double L2 = Math.Sqrt(Math.Pow((A2 - A1), 2) + Math.Pow((B2 - B1), 2));
-            Console.WriteLine("Length of line 1 is:: " + L2);
-            Console.WriteLine("-----------------------------");
+            Console.WriteLine("Length of L2 is:: " + L2);
+            Console.WriteLine("---------------------------------------------------");
             bool result = L1.Equals(L2);
             if (result == false)
             {
-                Console.WriteLine("Therefore, Lines L1 and L2 are NOT EQUAL.");
+                Console.WriteLine("Lines L1 and L2 are NOT EQUAL.");
             }
             else
             {
-                Console.WriteLine("Therefore, Lines L1 and L2 are EQUAL.");
+                Console.WriteLine("Lines L1 and L2 are EQUAL.");
             }
-            Console.WriteLine("-----------------------------");
+            Console.WriteLine("---------------------------------------------------");
+            double diff = L1.CompareTo(L2);
+            if (diff < 0)
+            {
+                Console.WriteLine("Length of Line L1 is less than L2.");
+            }
+            if (diff > 0)
+            {
+                Console.WriteLine("Length of Line L1 is greater than L2.");
+            }
+            if (diff == 0)
+            {
+                Console.WriteLine("Both Lines are equal in Length.");
+            }
+            Console.WriteLine("---------------------------------------------------");
             Console.WriteLine("Press any Key to Exit.");
             Console.ReadKey();
         }
